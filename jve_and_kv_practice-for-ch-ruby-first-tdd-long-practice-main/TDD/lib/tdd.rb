1f954cb59f726022f911a8de32a0df21c90475c5
Josh van Eyken <josh.vaneyken@gmail.com>
2023-02-23 15:26:03 -0800

    test cases for two-sum

--- jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/lib/tdd.rb
+++ jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/lib/tdd.rb
@@ -7,9 +7,14 @@ class Array
         new_arr
     end
 
+
+    def two_sum
+        # return an array of pairs
+        # iterate through all elements
+        # check if any two elements sum to zero
+            # if true, shovel in indices
+    end
+
 end
 
-# Array has a uniq method that removes duplicates from an array. It returns the unique elements in the order in which they first appeared:
 
-# [1, 2, 1, 3, 3].uniq # => [1, 2, 3]
-# Write your own version of this method called my_uniq; it should take in an array and return a new array.
--- jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/spec/tdd_spec.rb
+++ jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/spec/tdd_spec.rb
@@ -18,7 +18,33 @@ describe Array do
             expect(array.uniq).to eq([1, 2, 3])
         end
     end
+
+    describe '#two_sum' do
+        subject(:array) { [-1, 0, 2, -2, 1] }
+        it 'returns an empty array if no pairs are found' do
+            expect(array.two_sum).to eq([])
+        end
+
+        # if subarrays sum to zero
+        it 'returns an array of pairs that sum to zero' do
+            expect(array.two_sum.length).to eq(2)
+        end
+
+        it 'returns an array sorted "dictionary-wise"' do
+            expect(array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+    end
 end
+
+
+# Write a new Array#two_sum method that finds all pairs of positions where the elements at those positions sum to zero.
+
+# N.B.: Ordering matters. You want each of the pairs to be sorted with the smaller index before the bigger index. You then want the array of pairs to be sorted "dictionary-wise":
+
+# [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+# [0, 2] before [2, 1] (smaller first elements come first)
+# [0, 1] before [0, 2] (then smaller second elements come first)
 
 
 # describe "#mix!" do
@@ -37,8 +63,5 @@ end
 #   end
 
 
-# Array has a uniq method that removes duplicates from an array. It returns the unique elements in the order in which they first appeared:
 
-# [1, 2, 1, 3, 3].uniq # => [1, 2, 3]
-# Write your own version of this method called my_uniq; it should take in an array and return a new array.
 
