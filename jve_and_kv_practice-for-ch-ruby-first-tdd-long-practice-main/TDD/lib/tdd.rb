96ae1ebc9b9597136044325516dad7e1f0f3feac
Kathy Vu <vu.kathy98@gmail.com>
2023-02-23 17:33:16 -0800

    finish my_transpose method

--- jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/lib/tdd.rb
+++ jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/lib/tdd.rb
@@ -9,12 +9,25 @@ class Array
 
 
     def two_sum
-        # return an array of pairs
-        # iterate through all elements
-        # check if any two elements sum to zero
-            # if true, shovel in indices
+        pairs = []
+        (0...self.length).each do |i|
+            (0...self.length).each do |j|
+                pairs << [i, j] if j > i && self[i] + self[j] == 0
+            end
+        end
+        pairs
     end
 
+    def my_transpose
+        self.transpose
+        new_matrix = Array.new(self.length) {Array.new(self.length)}
+        (0...self.length).each do |i|
+            (0...self.length).each do |j|
+                new_matrix[i][j] = self[j][i]
+            end
+        end
+        new_matrix
+    end
 end
 
 
--- jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/spec/tdd_spec.rb
+++ jve_and_kv_practice-for-ch-ruby-first-tdd-long-practice-main/TDD/spec/tdd_spec.rb
@@ -21,30 +21,94 @@ describe Array do
 
     describe '#two_sum' do
         subject(:array) { [-1, 0, 2, -2, 1] }
+        subject(:array_none) { [1, 2, 3, 4, 5] }
+
         it 'returns an empty array if no pairs are found' do
-            expect(array.two_sum).to eq([])
+            expect(array_none.two_sum).to eq([])
         end
 
-        # if subarrays sum to zero
         it 'returns an array of pairs that sum to zero' do
             expect(array.two_sum.length).to eq(2)
+        end
+
+        it 'contains pairs that are sorted smaller index before the bigger index' do
+            first_pair = array.two_sum.first
+            expect(first_pair).to_not eq([4, 0])
         end
 
         it 'returns an array sorted "dictionary-wise"' do
             expect(array.two_sum).to eq([[0, 4], [2, 3]])
         end
+    end
 
+    describe '#my_transpose' do 
+        subject(:array) { [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ]
+        }
+
+        it 'must not use built-in Array#transpose' do
+            expect(array).to_not receive(:transpose)
+            array.my_transpose
+        end
+
+        it 'returns a matrix where rows and columns are switched' do
+            expect(array.my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+        end
     end
 end
 
+describe StockPicker do
+    [10000, 12000, 13000, 14000, 15000, 18000, 10000, 9000, 13000]
+    current_diff = 0
+    current_pair = []
+    #return a pair of days [day x, day y]
+    (0...self.length-1).each do |i|
+        (i+1...self.length).each do |j|
+            # if j > i 
+                if self[j] - self[i] > current_diff
+                    current_diff = self[j] - self[i]
+                    current_pair = [i, j]
+                end
+    #search for smallest number, look right for biggest number.
+    #if not on the right, 
 
-# Write a new Array#two_sum method that finds all pairs of positions where the elements at those positions sum to zero.
+end
 
-# N.B.: Ordering matters. You want each of the pairs to be sorted with the smaller index before the bigger index. You then want the array of pairs to be sorted "dictionary-wise":
+# arr_1 = [1,2,3,4]
+# it "should not use the built-in Array#map or Array#map!" do
+#     expect(arr_1).to_not receive(:map)
 
-# [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
-# [0, 2] before [2, 1] (smaller first elements come first)
-# [0, 1] before [0, 2] (then smaller second elements come first)
+# To represent a matrix, or two-dimensional grid of numbers, you can write an array containing arrays which represent rows:
+
+# rows = [
+#   [0, 1, 2],
+#   [3, 4, 5],
+#   [6, 7, 8]
+# ]
+
+# row1 = rows[0]
+# row2 = rows[1]
+# row3 = rows[2]
+# You could equivalently have stored the matrix as an array of columns:
+
+# cols = [
+#   [0, 3, 6],
+#   [1, 4, 7],
+#   [2, 5, 8]
+# ]
+# Write a method, my_transpose, which will convert between the row-oriented and column-oriented representations. You may assume square matrices for simplicity's sake. Usage will look like the following:
+
+# my_transpose([
+#   [0, 1, 2],
+#   [3, 4, 5],
+#   [6, 7, 8]
+# ])
+#  # => [[0, 3, 6],
+#  #     [1, 4, 7],
+#  #     [2, 5, 8]]
 
 
 # describe "#mix!" do
